c5caa7bad600fe659d61e88222570eef48c25648
Paola Pérez <Paola@MacBook-Pro-de-MACBOOK.local>
2017-10-27 10:30:02 -0500

    add:routing

--- page-idesmac/src/app/app-routing.module.ts
+++ page-idesmac/src/app/app-routing.module.ts
@@ -4,19 +4,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent} from './contacto/contacto.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
-    
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
     path: 'home',
-    children: [], 
     component: HomeComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
   }
-    
 ];
 
 @NgModule({
